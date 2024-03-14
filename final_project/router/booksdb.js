a39b5f3620d3a5927a3359af2a6a094068610fc1
JKRINGK <156469000+JKRINGK@users.noreply.github.com>
2024-03-14 12:07:40 -0600

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,13 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"user": "User1", "review": "It was an amazing book"}  },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {"user": "", "review": ""}  },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {"user": "", "review": ""}  },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {"user": "", "review": ""} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {"user": "", "review": ""} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {"user": "", "review": ""} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {"user": "", "review": ""} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {"user": "", "review": ""} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {"user": "", "review": ""} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"user": "user2", "review": "Existential exploration, fragmented narrative, profound reflection on human existence."} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": []  },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews":[]  },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": []  },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": [] },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": [] },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": [] },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": [] },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": [] },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": [] },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": [] }
 }
 module.exports=books;
+
