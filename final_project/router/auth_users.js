b22ea74259baa59a851d33a8bbc8ff18f4f2eef6
JKRINGK <156469000+JKRINGK@users.noreply.github.com>
2024-03-14 12:08:53 -0600

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,25 +5,100 @@ const regd_users = express.Router();
 
 let users = [];
 
+
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    let usersWithSameName = users.filter((user)=>{
+        return user.username === username
+      });
+      if(usersWithSameName.length > 0){
+        return true;
+      } else {
+        return false;
+      }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 3600 });
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+  });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let bookISBN = req.params.isbn;
+    let bookReview = req.body.review;
+    const loggedInUser = req.session.authorization.username;
+    
+    if (!books[bookISBN]) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    // Check if the user has been write a review
+    const existingReviewIndex = books[bookISBN].reviews.findIndex(review => review.user === loggedInUser);
+
+    if (existingReviewIndex !== -1) {
+        // Replace the last review
+        books[bookISBN].reviews[existingReviewIndex].review = bookReview;
+    } else {
+        // Add a new review
+        books[bookISBN].reviews.push({ user: loggedInUser, review: bookReview });
+    }
+
+    // Filter the review's user.
+    const reviewsUser = books[bookISBN].reviews.filter(review => review.user === loggedInUser);
+
+    res.send(reviewsUser);
 });
+
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let bookISBN = req.params.isbn;
+    const loggedInUser = req.session.authorization.username;
+    
+    if (!books[bookISBN]) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    // Check if the user has been write a review
+    const existingReviewIndex = books[bookISBN].reviews.findIndex(review => review.user === loggedInUser);
+
+    if (existingReviewIndex !== -1) {
+        // Replace the last review
+        books[bookISBN].reviews.splice(existingReviewIndex, 1);
+        return res.status(200).send("The review has been deleted.");
+    } else {
+        return res.status(404).json({ message: "The user has not written a review for book "+ bookISBN});
+    }
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
